0242b6e438975ffd3238d5783b5fe105207f8e8b
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-08-15 15:33:42 -0400

    fix: landing spacing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -16,12 +16,14 @@ export const Landing = () => {
             loop={true}
             style={{ height: "550px" }}
           />
-          <h1>All your files in one secure location, accessible anywhere.</h1>
-          <p>
-            Fylo stores all your most important files in one secure location.
-            Access them wherever you need, share and collaborate with friends,
-            family and co-workers.
-          </p>
+          <div>
+            <h1>All your files in one secure location, accessible anywhere.</h1>
+            <p>
+              Fylo stores all your most important files in one secure location.
+              Access them wherever you need, share and collaborate with friends,
+              family and co-workers.
+            </p>
+          </div>
           <a href="/" className="blueBtn" onClick={(e) => e.preventDefault()}>
             Get Started
           </a>
